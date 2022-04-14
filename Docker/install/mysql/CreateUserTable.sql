27ae8e556df214d11ef2a16abbf1ce0359d7ca6a
yongkim_com2us <yongkim@com2us.com>
2022-04-14 13:27:17 +0900

    Docker compose 작성
    Mysql, Redis Docker 컨테이너 빌드 및 실행 파일 작성

---
+++ Docker/install/mysql/CreateUserTable.sql
@@ -0,0 +1,16 @@
+CREATE SCHEMA IF NOT EXISTS `YONGJIN`;
+USE YONGJIN;
+
+CREATE TABLE `Users`(
+    userID varchar(50) NOT NULL PRIMARY KEY,  
+    salt varchar(50),
+    hashingPW varchar(50)
+);
+
+CREATE TABLE `MailBox`(
+    MailBoxID varchar(50) NOT NULL PRIMARY KEY,
+    senderID varchar(50),
+    kind int,
+    Item varchar(50),
+    ItemCount int
+);
