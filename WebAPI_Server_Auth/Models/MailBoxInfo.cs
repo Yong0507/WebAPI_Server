e9a0e546eb1c76ddb61cd61b4330e0491fd727f2
yongkim_com2us <62415286+Yong0507@users.noreply.github.com>
2022-04-19 13:28:41 +0900

    EF Core 정의
    EF Core 정의

---
+++ WebAPI_Server_Auth/Models/MailBoxInfo.cs
@@ -0,0 +1,11 @@
+﻿namespace WebAPI_Server.Models
+{
+    public class MailBoxInfo
+    {
+        public string MailBoxID { get; set; }
+        public string senderID { get; set; }
+        public int kind { get; set; }
+        public string Item { get; set; }
+        public int ItemCount { get; set; }
+    }
+}
