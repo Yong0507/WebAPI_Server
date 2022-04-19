a34f49ccc998ffae1cfff9193e935eefc2b1b993
yongkim_com2us <62415286+Yong0507@users.noreply.github.com>
2022-04-19 14:56:51 +0900

    삭제
    삭제

--- WebAPI_Server_Auth/Models/AccountInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace WebAPI_Server.Models
-{
-    public class AccountInfo
-    {
-        public string userID { get; set; }
-        public string salt { get; set; }
-        public string hashingPW { get; set; }
-    }
-}
